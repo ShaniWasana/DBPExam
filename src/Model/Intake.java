f1d2c9f2b65c0927dc8fe1f1ea74b07634ac65f2
ShaniWasana <rukshaniwasana59@gmail.com>
2022-07-12 09:30:19 +0530

    Updated the intake model

--- src/Model/Intake.java
+++ src/Model/Intake.java
@@ -5,17 +5,15 @@ import java.util.Date;
 public class Intake {
     private String Iid;
     private Date Start_Date;
-    private String Intake_Col;
     private String Description;
     private String CId;
 
     public Intake() {
     }
 
-    public Intake(String iid, Date start_Date, String intake_Col, String description, String CId) {
+    public Intake(String iid, Date start_Date, String description, String CId) {
         Iid = iid;
         Start_Date = start_Date;
-        Intake_Col = intake_Col;
         Description = description;
         this.CId = CId;
     }
@@ -34,14 +32,6 @@ public class Intake {
 
     public void setStart_Date(Date start_Date) {
         Start_Date = start_Date;
-    }
-
-    public String getIntake_Col() {
-        return Intake_Col;
-    }
-
-    public void setIntake_Col(String intake_Col) {
-        Intake_Col = intake_Col;
     }
 
     public String getDescription() {
@@ -65,7 +55,6 @@ public class Intake {
         return "Intake{" +
                 "Iid='" + Iid + '\'' +
                 ", Start_Date=" + Start_Date +
-                ", Intake_Col='" + Intake_Col + '\'' +
                 ", Description='" + Description + '\'' +
                 ", CId='" + CId + '\'' +
                 '}';
